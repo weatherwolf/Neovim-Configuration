86fd92cc98f4887af0370504a3db3b43d6b1479f
Weatherwolf <wolfbekkers@gmail.com>
2026-03-31 14:04:10 +0200

    small change for linux setup 2

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ fi
 
 mkdir -p ~/.config/nvim
 
-git clone https://github.com/weatherwolf/Neovim-Configuration </dev/null
+git clone --depth=1 --branch main https://github.com/weatherwolf/Neovim-Configuration </dev/null
 mv Neovim-Configuration/init.lua ~/.config/nvim/
 mv Neovim-Configuration/lazy-lock.json ~/.config/nvim/
 rm -rf Neovim-Configuration
